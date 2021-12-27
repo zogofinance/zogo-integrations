cc6f1ec4a8eba06336c78e3b644c671cc61a1c90
Derek Miller <dsymiller@gmail.com>
2021-12-27 17:07:48 -0500

    initial sandbox-template

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  arrowParens: 'avoid',
+};
